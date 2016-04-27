75aa6dc1527b90e709fae96c8f761d5b79084489
RIdotCOM <richardivan.com@gmail.com>
2016-04-27 13:11:17 +0200

    add URI library as dev dependency

--- app/js/services/protoService.js
+++ app/js/services/protoService.js
@@ -1,12 +1,18 @@
 'use strict'
 
+import URI from 'urijs'
+
 const ProtoBuf = require('protobufjs')
 
 const builder = ProtoBuf.loadProtoFile('../../../gtfs-realtime.proto')
 
 const RealTime = builder.build('transit_realtime')
 
-const url = 'http://api.bart.gov/gtfsrt/tripupdate.aspx'
+// const url = 'http://api.bart.gov/gtfsrt/tripupdate.aspx'
+
+let url = URI(window.location.href)
+console.log(url)
+// const url = 'http://api.bart.gov/gtfsrt/tripupdate.aspx'
 
 export default function() {
 
